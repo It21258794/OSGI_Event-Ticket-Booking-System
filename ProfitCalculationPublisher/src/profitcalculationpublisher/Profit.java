5c9bef48008f39695b484e70eb13e729765dd394
IT21174308 <it21174308@my.sliit.lk>
2024-03-06 19:21:39 +0530

    profitCalculate

---
+++ ProfitCalculationPublisher/src/profitcalculationpublisher/Profit.java
@@ -0,0 +1,100 @@
+package profitcalculationpublisher;
+
+public class Profit {
+
+	private int calId;
+	private String eventName;
+	private int soldTickets;
+	private double ticketPrice;
+	private double income;
+	private double cost;
+	private double profit;
+	
+	
+	/**
+	 * @return the calId
+	 */
+	public int getCalId() {
+		return calId;
+	}
+	/**
+	 * @param calId the calId to set
+	 */
+	public void setCalId(int calId) {
+		this.calId = calId;
+	}
+	/**
+	 * @return the eventName
+	 */
+	public String getEventName() {
+		return eventName;
+	}
+	/**
+	 * @param eventName the eventName to set
+	 */
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	/**
+	 * @return the soldTickets
+	 */
+	public int getSoldTickets() {
+		return soldTickets;
+	}
+	/**
+	 * @param soldTickets the soldTickets to set
+	 */
+	public void setSoldTickets(int soldTickets) {
+		this.soldTickets = soldTickets;
+	}
+	/**
+	 * @return the ticketPrice
+	 */
+	public double getTicketPrice() {
+		return ticketPrice;
+	}
+	/**
+	 * @param ticketPrice the ticketPrice to set
+	 */
+	public void setTicketPrice(double ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+	/**
+	 * @return the income
+	 */
+	public double getIncome() {
+		return income;
+	}
+	/**
+	 * @param income the income to set
+	 */
+	public void setIncome(double income) {
+		this.income = income;
+	}
+	/**
+	 * @return the cost
+	 */
+	public double getCost() {
+		return cost;
+	}
+	/**
+	 * @param cost the cost to set
+	 */
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	/**
+	 * @return the profit
+	 */
+	public double getProfit() {
+		return profit;
+	}
+	/**
+	 * @param profit the profit to set
+	 */
+	public void setProfit(double profit) {
+		this.profit = profit;
+	}
+	
+	
+	}
